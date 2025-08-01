3af45570ea52e994ea884aaadf8cf26242531881
Alexander Ershov <lancaster1@ro.ru>
2025-08-01 13:38:53 +0300

    add pay form

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
 function App() {
   return (
-    <div className="w-[100%] h-[100%] flex flex-col items-center">
-      <div className="w-[457px] h-[464px]">
-        <h1 className="text-title">Оплата банковской картой</h1>
-        <p>Номер карты</p>
-        <input type="text" placeholder="0000 0000 0000 0000" />
-        <div>
-          <p>Месяц/Год</p>
-          <input type="text" placeholder="Default" />
-          <p>Код</p>
-          <input type="text" placeholder="***" />
+    <div className="w-[100%] h-[100%] flex flex-col items-center justify-center">
+      <div className="w-[457px] h-[464px] pl-[20px] border border-[#d9dee2] rounded-[10px]">
+        <h1 className="text-title mt-[32px] w-[368px] h-[32px]">
+          Оплата банковской картой
+        </h1>
+        <div className="h-[84px] w-[417px] mt-[20px]">
+          <p className="text-grey-800 text-button">Номер карты</p>
+          <input
+            className="w-[417px] h-[40px] mt-[4px] rounded-[10px] pl-[14px] border border-[#d9dee2] hover:border-[#d9dee2] focus:border-[#d9dee2] focus:outline-none"
+            type="text"
+            placeholder="0000 0000 0000 0000"
+          />
         </div>
-        <p>Владелец карты</p>
-        <input type="text" placeholder="IVAN IVANOV" />
-        <button className="">Оплатить</button>
+        <div className="flex gap-[77px] mt-[20px]">
+          <div className="h-[84px] w-[170px]">
+            <p className="text-grey-800 text-button">Месяц/Год</p>
+            <input
+              className="w-[170px] h-[40px] rounded-[10px] mt-[4px] pl-[14px] border border-[#d9dee2] hover:border-[#d9dee2] focus:border-[#d9dee2] focus:outline-none"
+              type="text"
+              placeholder="Default"
+            />
+          </div>
+          <div className="h-[84px] w-[170px]">
+            <p className="text-grey-800 text-button">Код</p>
+            <input
+              className="w-[170px] h-[40px] rounded-[10px] mt-[4px] pl-[14px] border border-[#d9dee2] hover:border-[#d9dee2] focus:border-[#d9dee2] focus:outline-none"
+              type="text"
+              placeholder="***"
+            />
+          </div>
+        </div>
+        <div className="mt-[20px] h-[84px] w-[417px]">
+          <p className="text-grey-800 text-button">Владелец карты</p>
+          <input
+            className="w-[417px] h-[40px] rounded-[10px] pl-[14px] mt-[4px] border border-[#d9dee2] hover:border-[#d9dee2] focus:border-[#d9dee2] focus:outline-none"
+            type="text"
+            placeholder="IVAN IVANOV"
+          />
+        </div>
+        <div className="mt-[20px] flex justify-end pr-[20px]">
+          <button className="w-[123px] h-[48px] rounded-[10px] bg-grey-100 cursor-pointer">
+            Оплатить
+          </button>
+        </div>
       </div>
     </div>
   );
